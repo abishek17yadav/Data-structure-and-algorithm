f729e517646b8ad2189eca3591246d47796d4fa8
Abishek yadav <abhiyadav172408@gmail.com>
2023-09-29 22:35:44 +0530

    Add files via upload
    Creating a node

---
+++ CreatingNodell1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+	public:
+		int val;
+		Node* next;
+		
+		Node(int data){
+			val=data;
+			next=NULL;
+		}
+};
+int main(){
+	Node* node1=new Node(1);
+	cout<<node1->val<<endl;
+	cout<<node1->next<<endl;
+	return 0;
+}
