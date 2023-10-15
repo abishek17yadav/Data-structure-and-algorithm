82d22183bfa89460e64cc86a44b61373bffa0619
Abishek yadav <abhiyadav172408@gmail.com>
2023-10-15 23:16:26 +0530

    Add files via upload

---
+++ Stack1.cpp
@@ -0,0 +1,69 @@
+//implementing stacks using array
+
+#include<iostream>
+using namespace std;
+class Stack{
+	int capacity;
+	int* arr;
+	int top;
+	
+	public:
+	Stack(int c){
+		this->capacity=c;
+		arr=new int(c);
+		this->top=-1;
+			}
+		
+	void push(int data){
+		if(this->top==this->capacity-1){
+			cout<<"Overflow";
+			return;	
+		}
+		this->top++;
+		this->arr[this->top]=data;
+	}
+	
+	int pop(){
+		if(this->top=-1){
+			cout<<"Underflow";
+			return INT_MIN;
+		}
+		this->top--;
+	}
+	
+	int gettop(){
+		if(this->top==-1){
+			cout<<"Underflow";
+			return INT_MIN;
+		}
+		return this->arr[this->top];
+	} 
+	
+	bool isempty(){
+		return this->top==-1;
+	}
+
+	int size(){
+		return this->top+1;
+		
+	}
+	
+	bool isfull(){
+		return this->top==this->capacity-1;
+	}
+	
+	
+};
+
+int main(){
+	Stack st(5);
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	cout<<st.gettop()<<"\n";		
+	st.push(4);
+	st.push(5);
+	cout<<st.gettop()<<"\n";
+	st.push(8);
+	return 0;
+}
